204c478df2d0144dc6468caf0af82be083bbb66c
JamesAllen <jayallen51@hotmail.co.uk>
2021-05-25 10:15:34 +0100

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-//--------------------------------------------------------------------
-// Modules
-module "baseinfra" {
-  source  = "app.terraform.io/JamesAllenOrg/baseinfra/google"
-  version = "0.0.5"
-
-  instance_count = 1
-  project_name = "appdev"
-}
